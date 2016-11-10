b88c4c4c11233463f500032310561719cf0a8bd3
Sinx <HeabKing@qq.com>
2016-11-10 18:28:50 +0800

    fix bug that not create content in get method but set content headers

--- src/Sinx.Utility/Extension/System.Net.Http.HttpRequestMessageEx.cs
+++ src/Sinx.Utility/Extension/System.Net.Http.HttpRequestMessageEx.cs
@@ -9,103 +9,100 @@ using Sinx.Utility.Extension;
 // ReSharper disable once CheckNamespace
 namespace System.Net.Http
 {
-    public static class HttpRequestMessageEx
-    {
-        /// <summary>
+	public static class HttpRequestMessageEx
+	{
+		/// <summary>
 		/// 根据原始的Http请求字符生成HttpRequestMessage
 		/// </summary>
 		/// <param name="request">用于表示拓展方法的this</param>
 		/// <param name="reqRaw">原始Http请求字符</param>
 		/// <returns></returns>
-        // ReSharper disable once MemberCanBePrivate.Global
+		// ReSharper disable once MemberCanBePrivate.Global
 		public static HttpRequestMessage CreateFromRaw(this HttpRequestMessage request, string reqRaw)
-        {
-            // 解析reqRaw
-            var splitLine = Regex.Split(reqRaw.Trim(), Environment.NewLine).Select(m => m.Trim()).ToList();
-            #region 1. 解析请求行
-            // 1. 解析请求行
-            var requestLine = Regex.Split(splitLine.FirstOrDefault() ?? "", "\\s+");
-            if (requestLine.Count() != 3 ||
-                !RegexEx.IsUrl(requestLine[1]) ||
-                !Regex.IsMatch(requestLine[2].ToLower(), @"http/\d+\.\d+"))
-            {
-                throw new ArgumentException("请求行解析出错");
-            }
-            var httpMethod = requestLine[0].Trim();
-            var httpUrl = requestLine[1].Trim();
-            var httpVersion = requestLine[2].Trim();    // 这里先使用默认的, 不用reqRaw中的
-            request.Method = new HttpMethod(httpMethod);
-            request.RequestUri = new Uri(httpUrl);
-            splitLine.Remove(splitLine.First());
-            #endregion
-            #region 2. 解析请求体
-            // 2. 解析请求体
-            if (httpMethod.ToLower() != "get")
-            {
-                var contentFlag = splitLine.FirstOrDefault(string.IsNullOrWhiteSpace);
-                var indexFlag = splitLine.IndexOf(contentFlag);
-                string content = "";
-                if (contentFlag != null)
-                {
-                    for (int i = indexFlag; i < splitLine.Count; i++)
-                    {
-                        content += splitLine[i];
-                    }
-                    splitLine.RemoveRange(indexFlag, splitLine.Count - indexFlag);
-                }
-                request.Content = new StringContent(content);
-                // 我截取的Content内容可能不跟reqRaw中的完全长度一样, 所以还是依赖框架自己计算Content-Length吧
-                splitLine = splitLine.Where(m => !Regex.IsMatch(m, "Content-Length", RegexOptions.IgnoreCase)).ToList();
-            }
-            #endregion
-            // 3. 解析请求头
-            foreach (var keyvalue in splitLine)
-            {
-                var keyValue = keyvalue.Split(":".ToCharArray(), 2);
-                var key = keyValue.FirstOrDefault()?.Trim();
-                var value = keyValue.LastOrDefault()?.Trim();
-                if (key == null)
-                {
-                    throw new ArgumentException("请求头解析出错");
-                }
-                // 先尝试添加到HttpRequestHeaders中, 如果不行尝试添加到HttpContentHeaders中
-                var removeOk = false;
-                try
-                {
-                    request.Headers.Remove(key);
-                    removeOk = true;
-                }
-                catch (Exception)
-                {
-                    // ignored
-                }
-                // 如果可以添加到HttpRequestHeaders中
-                if (removeOk && request.Headers.TryAddWithoutValidation(key, value)) continue;
-                // 尝试添加到HttpContentHeaders中
-                removeOk = false;
-                try
-                {
-                    request.Content.Headers.Remove(key);
-                    removeOk = true;
-                }
-                catch (Exception)
-                {
-                    // ignored
-                }
-                if (!removeOk || !request.Content.Headers.TryAddWithoutValidation(key, value))
-                {
-                    throw new ArgumentException(nameof(key));
-                }
-            }
-            return request;
-        }
-        public static HttpRequestMessage CreateFromRaw(string reqRaw)
-        {
-            var reuqest = new HttpRequestMessage();
-            return CreateFromRaw(reuqest, reqRaw);
-        }
+		{
+			// 解析reqRaw
+			var splitLine = Regex.Split(reqRaw.Trim(), Environment.NewLine).Select(m => m.Trim()).ToList();
+			#region 1. 解析请求行
+			// 1. 解析请求行
+			var requestLine = Regex.Split(splitLine.FirstOrDefault() ?? "", "\\s+");
+			if (requestLine.Count() != 3 ||
+				!RegexEx.IsUrl(requestLine[1]) ||
+				!Regex.IsMatch(requestLine[2].ToLower(), @"http/\d+\.\d+"))
+			{
+				throw new ArgumentException("请求行解析出错");
+			}
+			var httpMethod = requestLine[0].Trim();
+			var httpUrl = requestLine[1].Trim();
+			var httpVersion = requestLine[2].Trim();    // 这里先使用默认的, 不用reqRaw中的
+			request.Method = new HttpMethod(httpMethod);
+			request.RequestUri = new Uri(httpUrl);
+			splitLine.Remove(splitLine.First());
+			#endregion
+			#region 2. 解析请求体
+			// 2. 解析请求体
+			var contentFlag = splitLine.FirstOrDefault(string.IsNullOrWhiteSpace);
+			var indexFlag = splitLine.IndexOf(contentFlag);
+			string content = "";
+			if (contentFlag != null)
+			{
+				for (int i = indexFlag; i < splitLine.Count; i++)
+				{
+					content += splitLine[i];
+				}
+				splitLine.RemoveRange(indexFlag, splitLine.Count - indexFlag);
+			}
+			request.Content = new StringContent(content);
+			// 我截取的Content内容可能不跟reqRaw中的完全长度一样, 所以还是依赖框架自己计算Content-Length吧
+			splitLine = splitLine.Where(m => !Regex.IsMatch(m, "Content-Length", RegexOptions.IgnoreCase)).ToList();
+			#endregion
+			// 3. 解析请求头
+			foreach (var keyvalue in splitLine)
+			{
+				var keyValue = keyvalue.Split(":".ToCharArray(), 2);
+				var key = keyValue.FirstOrDefault()?.Trim();
+				var value = keyValue.LastOrDefault()?.Trim();
+				if (key == null)
+				{
+					throw new ArgumentException("请求头解析出错");
+				}
+				// 先尝试添加到HttpRequestHeaders中, 如果不行尝试添加到HttpContentHeaders中
+				var removeOk = false;
+				try
+				{
+					request.Headers.Remove(key);
+					removeOk = true;
+				}
+				catch (Exception)
+				{
+					// ignored
+				}
+				// 如果可以添加到HttpRequestHeaders中
+				if (removeOk && request.Headers.TryAddWithoutValidation(key, value)) continue;
+				// 尝试添加到HttpContentHeaders中
+				removeOk = false;
+				try
+				{
+					request.Content.Headers.Remove(key + " : " + keyvalue);
+					removeOk = true;
+				}
+				catch (Exception)
+				{
+					// ignored
+				}
+				if (!removeOk || !request.Content.Headers.TryAddWithoutValidation(key, value))
+				{
+					throw new ArgumentException(nameof(key));
+				}
+			}
+			return request;
+		}
+		public static HttpRequestMessage CreateFromRaw(string reqRaw)
+		{
+			var reuqest = new HttpRequestMessage();
+			return CreateFromRaw(reuqest, reqRaw);
+		}
 
-        /// <summary>
+		/// <summary>
 		/// http://stackoverflow.com/questions/25044166/how-to-clone-a-httprequestmessage-when-the-original-request-has-content?noredirect=1#comment38953745_25044166
 		/// http://stackoverflow.com/questions/18000583/re-send-httprequestmessage-exception/18014515#18014515
 		/// http://stackoverflow.com/questions/25047311/the-request-message-was-already-sent-cannot-send-the-same-request-message-multi
@@ -114,33 +111,33 @@ namespace System.Net.Http
 		/// <param name="req"></param>
 		/// <returns></returns>
 		public static async Task<HttpRequestMessage> CloneAsync(this HttpRequestMessage req)
-        {
-            HttpRequestMessage clone = new HttpRequestMessage(req.Method, req.RequestUri);
+		{
+			HttpRequestMessage clone = new HttpRequestMessage(req.Method, req.RequestUri);
 
-            // Copy the request's content (via a MemoryStream) into the cloned object
-            var ms = new MemoryStream();
-            if (req.Content != null)
-            {
-                await req.Content.CopyToAsync(ms).ConfigureAwait(false);
-                ms.Position = 0;
-                clone.Content = new StreamContent(ms);
+			// Copy the request's content (via a MemoryStream) into the cloned object
+			var ms = new MemoryStream();
+			if (req.Content != null)
+			{
+				await req.Content.CopyToAsync(ms).ConfigureAwait(false);
+				ms.Position = 0;
+				clone.Content = new StreamContent(ms);
 
-                // Copy the content headers
-                if (req.Content.Headers != null)
-                    foreach (var h in req.Content.Headers)
-                        clone.Content.Headers.Add(h.Key, h.Value);
-            }
+				// Copy the content headers
+				if (req.Content.Headers != null)
+					foreach (var h in req.Content.Headers)
+						clone.Content.Headers.Add(h.Key, h.Value);
+			}
 
 
-            clone.Version = req.Version;
+			clone.Version = req.Version;
 
-            foreach (KeyValuePair<string, object> prop in req.Properties)
-                clone.Properties.Add(prop);
+			foreach (KeyValuePair<string, object> prop in req.Properties)
+				clone.Properties.Add(prop);
 
-            foreach (KeyValuePair<string, IEnumerable<string>> header in req.Headers)
-                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+			foreach (KeyValuePair<string, IEnumerable<string>> header in req.Headers)
+				clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
 
-            return clone;
-        }
-    }
+			return clone;
+		}
+	}
 }
